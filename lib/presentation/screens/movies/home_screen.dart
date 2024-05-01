177449eb6cb284ab4ee2f2b4fd8fcaf805e213c1
Arturo Sandoval <arsabe75@gmail.com>
2024-04-30 19:54:27 -0600

    Movies Horizontal ListView y mostrar películas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -36,7 +36,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    //final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
     // Temporal
@@ -50,6 +50,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
       children: [
         const CustomAppbar(),
         MoviesSlideshow(movies: slideShowMovies),
+        MoviesHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En Cines',
+          subTitle: 'Lunes 20',
+        )
       ],
     );
   }
---
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -0,0 +1,147 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+class MoviesHorizontalListview extends StatelessWidget {
+  final List<Movie> movies;
+  final String? title;
+  final String? subTitle;
+  final VoidCallback? loadNextPage;
+
+  const MoviesHorizontalListview({
+    super.key,
+    required this.movies,
+    this.title,
+    this.subTitle,
+    this.loadNextPage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 350,
+      child: Column(
+        children: [
+          if (title != null || subTitle != null)
+            _Title(title: title, subTitle: subTitle),
+          const SizedBox(height: 5),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: movies.length,
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                final movie = movies[index];
+                return _Slide(movie: movie);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Esto es la imagen:
+          SizedBox(
+            width: 150,
+            height: 220,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) {
+                    return const DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black12),
+                    );
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ),
+          const SizedBox(width: 5),
+          // Title:
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              style: textStyles.titleSmall,
+            ),
+          ),
+          // Rating:
+          Row(
+            children: [
+              Icon(
+                Icons.star_half_outlined,
+                size: 20,
+                color: Colors.yellow.shade800,
+              ),
+              const SizedBox(width: 3),
+              Text(
+                '${movie.voteAverage}',
+                style: textStyles.bodyMedium?.copyWith(
+                  color: Colors.yellow.shade800,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Text(
+                '${movie.popularity}',
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  final String? title;
+  final String? subTitle;
+
+  const _Title({this.title, this.subTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Row(
+        children: [
+          Text(
+            title ?? '',
+            style: titleStyle,
+          ),
+          const Spacer(),
+          FilledButton.tonal(
+            style: const ButtonStyle(visualDensity: VisualDensity.compact),
+            onPressed: () {},
+            child: Text(subTitle ?? ''),
+          ),
+        ],
+      ),
+    );
+  }
+}
