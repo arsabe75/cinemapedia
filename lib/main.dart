9423b8acca373d078d5feaff697b3ec71d42801d
Arturo Sandoval <arsabe75@gmail.com>
2024-08-07 13:38:04 -0600

    Modificación mouse drag desktop + Window Manager Size

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
+  if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = const WindowOptions(
+      size: Size(380, 840),
+      minimumSize: Size(380, 840),
+      maximumSize: Size(380, 840),
+      center: true,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      //titleBarStyle: TitleBarStyle.normal,
+      title: 'Cinemapedia ASB',
+    );
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
+
   await dotenv.load(fileName: ".env");
 
   runApp(
@@ -20,6 +44,14 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown
+        },
+      ),
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
       theme: AppTheme().getTheme(),
