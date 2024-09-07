fbb3378ed109a892f4878c17b15595759315f4c9
Arturo Sandoval <arsabe75@gmail.com>
2024-09-07 14:39:22 -0600

    Continuación app + Navegar a otra pantalla con parámetros

--- lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -1,7 +1,9 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_formats.dart';
+import 'package:cinemapedia/config/router/app_router.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class MoviesHorizontalListview extends StatefulWidget {
   final List<Movie> movies;
@@ -100,7 +102,9 @@ class _Slide extends StatelessWidget {
                       decoration: BoxDecoration(color: Colors.black12),
                     );
                   }
-                  return FadeIn(child: child);
+                  return GestureDetector(
+                      onTap: () => context.push('/movie/${movie.id}'),
+                      child: FadeIn(child: child));
                 },
               ),
             ),
