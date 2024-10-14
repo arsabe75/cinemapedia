d5d0b7b3c93526ea6da5dfa082c9309796c9b941
Arturo Sandoval <arsabe75@gmail.com>
2024-10-14 12:00:05 -0600

    Bottom Navigation Bar - GoRouter básico

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AppTheme {
   ThemeData getTheme() => ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: const Color(0xff2862F5),
+        colorSchemeSeed: Colors.blue, //Color(0xff2862F5),
       );
 }
