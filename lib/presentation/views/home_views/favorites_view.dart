86b951c3c6b31c41d23bef3212c171cb533510bc
Arturo Sandoval <arsabe75@gmail.com>
2024-10-19 14:40:23 -0600

    Fin de app explicada en video - Falta modificación adicional 2o view.

--- lib/presentation/views/home_views/favorites_view.dart
+++ lib/presentation/views/home_views/favorites_view.dart
@@ -1,5 +1,4 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
-import 'package:cinemapedia/presentation/widgets/shared/custom_appbar.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
